3be680feffba59f4075cf6d16ec2c9dbf9a5d41b
Natalie <favoritewillow@gmail.com>
2023-08-16 19:19:37 +0300

    feat: add brackets (Wednesday 16.08.2023 19:19pm)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openBrackets = new Set();
+  const bracketPairs = {};
+
+  for (const [open, close] of bracketsConfig) {
+    openBrackets.add(open);
+    bracketPairs[close] = open;
+  }
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      if (char === bracketPairs[char] && stack[stack.length - 1] === char) {
+        stack.pop(); 
+      } else {
+        stack.push(char);
+      }
+    } else {
+      const openBracket = bracketPairs[char];
+      if (
+        openBracket !== undefined &&
+        stack[stack.length - 1] === openBracket
+      ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+    
 }
